e2437976726a3b1329c70ced33ec01d8f24a97f2
[Pawan Gupta] <[pawanguptaa43@gmail.com]>
2025-07-24 13:35:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Pawan!')
+})
+
+app.get('/journey',(req,res)=>{
+      res.send("BECOMING STRONGER DAY BY DAY")
+})
+
+app.get('/promise',(req,res)=>{
+    res.send("ENCODED")
+})
+app.listen(porprocess.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
